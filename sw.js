098fe362e9a8ad5310b864f0ee5cecd9909a1105
0xjaqbek <jaqbek.eth@gmail.com>
2026-03-06 09:57:08 +0100

    Add service worker and caching for offline support; include app icons in manifest

---
+++ sw.js
@@ -0,0 +1,24 @@
+var CACHE = 'kmcr-v1';
+var URLS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(c) { return c.addAll(URLS); })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.filter(function(k) { return k !== CACHE; }).map(function(k) { return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) { return r || fetch(e.request); })
+  );
+});
